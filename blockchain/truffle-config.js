61ada009a29255cd50ad95753e61030659e25d41
Maha Abid <abidmaha1998@gmail.com>
2024-01-10 13:31:41 +0100

    first commit

---
+++ blockchain/truffle-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+      gas: 30000000,
+      from: '0x21747c23E743ddBF4495C802A1899835602dF17e',
+    },
+  },
+  contracts_directory: './testing/',
+  contracts_build_directory: './src/build/',
+  compilers: {
+    solc: {
+      version: "0.6.6",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 100
+        }
+      }
+    }
+  }
+}
